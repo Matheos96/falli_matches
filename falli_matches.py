88ed588a1139b82366515e1c4327df0da08ffe5d
Matheos Mattsson <32064310+Matheos96@users.noreply.github.com>
2022-07-12 15:33:13 +0300

    Refactorings and separation of setup to separate file
    - Added type hints
    - Changes all occurences of the word "game" to "match"
    - Moved setup logic to its own file
    - New log entries are now added at the top of the log file

--- falli_matches.py
+++ falli_matches.py
@@ -1,98 +1,81 @@
-from os.path import exists
-from pandas import json_normalize
-from cookie import cookie  # Import cookie from cookie.py. This file needs to be manually created by the user (see README.md) 
 import requests
 import datetime
+import setup
 import pandas as pd
-
-# SETUP
-start_date = '2022-07-08'
-url = 'https://spl.torneopal.net/taso/rest/getMatches'
-querystring = {'start_date':start_date,'venue_location_id':'2397','tpid':'1667178516'}
-headers = {
-    "cookie": cookie,
-    "authority": "spl.torneopal.net",
-    "accept": "json/df8e84j9xtdz269euy3h",
-    "accept-language": "sv-SE,sv;q=0.9,en-US;q=0.8,en;q=0.7",
-    "if-modified-since": "Thu, 01 Jan 1970 00:00:00 GMT",
-    "if-none-match": "^\^44ed893f7b8d2f86df14e300ab21b2a5^^",
-    "origin": "https://tulospalvelu.palloliitto.fi",
-    "referer": "https://tulospalvelu.palloliitto.fi/",
-    "sec-ch-ua": "^\^.Not/A",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "^\^Windows^^",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "cross-site",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
-}
-previous_data_file = 'prev.pkl'
-log_file = 'Falli_matches_log.txt'
+from os.path import exists
+from pandas import json_normalize
 
 # Compiles a list of "game rows" given a dataframe containing games
-def get_game_rows(df: pd.DataFrame):
+def generate_match_rows(df: pd.DataFrame) -> list[str]:
     return [f'{row["date"]} | {row["time"]} | {row["venue_name"]} | {row["category_name"]}: {row["team_A_name"]} vs. {row["team_B_name"]}\n' for _, row in df.iterrows()]
 
 # Prints a list of strings. Each string on its own row
-def print_rows(rows):
+def print_rows(rows: list[str]) -> None:
     for r in rows:
         print(r)
 
-# Writes a list of rows to the log file
-def write_rows_to_log(rows):
-    with open(log_file, 'a+') as file:
+# Writes a list of rows to the top of the log file
+def write_rows_to_log(rows: list[str]) -> None:
+    with open(setup.log_file, 'r') as file:
+        old_content = file.read()
+    with open(setup.log_file, 'w') as file:
         file.write(f'---------------------------- FROM SESSION: {datetime.datetime.now()} ----------------------------\n')
         file.writelines(rows)
         file.write('\n')
+    with open(setup.log_file, 'a') as file:
+        file.write(old_content)
 
-if __name__ == '__main__':
+def main() -> None:
     # Fetch Json data
     with requests.Session() as s:
-        r = s.request('GET', url, data='', headers=headers, params=querystring)
+        r = s.request('GET', setup.url, data='', headers=setup.headers, params=setup.querystring)
         json_result = r.json()
 
     # If we got data to work with
     if json_result and 'matches' in json_result:
-        # Put the matches Json array into a pandas dataframe
-        df = json_normalize(json_result['matches'])
+        df = json_normalize(json_result['matches'])  # Put the matches Json array into a pandas dataframe
         df = df[['date', 'time', 'category_name', 'venue_name', 'team_A_name', 'team_B_name']]  # Only pick interesting columns
 
-        to_log = []  # List to keep all rows to be written to log
+        session_log = []
 
-        # Read previous result from file to dataframe (if file exists)
-        if exists(previous_data_file):
-            prev_df = pd.read_pickle(previous_data_file)
-
-            # Merge previous and current
-            merged = df.merge(prev_df, how='outer', indicator=True)
+        # If a previous data cache exists, compare and report the result
+        if exists(setup.previous_data_file):
+            prev_df = pd.read_pickle(setup.previous_data_file)  # Read previous result from file to dataframe 
+            merged = df.merge(prev_df, how='outer', indicator=True)  # Merge previous (cached data) dataframe and current dataframe
 
             # Find new entries
             news = merged.loc[lambda x : x['_merge']=='left_only']
-            new_game_rows = get_game_rows(news)
-            if len(new_game_rows) > 0:
-                to_log.append('New games since last run:\n')
-                to_log.extend(new_game_rows)
+            new_match_rows = generate_match_rows(news)
+            if len(new_match_rows) > 0:
+                session_log.append('New matches since last run:\n')
+                session_log.extend(new_match_rows)
 
             # Find removed entries
             removed = merged.loc[lambda x : x['_merge']=='right_only']
-            removed_game_rows = get_game_rows(removed)
-            if len(removed_game_rows) > 0:
-                to_log.append('\nRemoved games since last run:\n')
-                to_log.extend(removed_game_rows)
+            removed_match_rows = generate_match_rows(removed)
+            if len(removed_match_rows) > 0:
+                session_log.append('\nRemoved matches since last run:\n')
+                session_log.extend(removed_match_rows)
             
-            if len(removed_game_rows) == 0 and len(new_game_rows) == 0:
-                to_log.append('No changes since last time!')
+            # If no changes were found, make the user aware
+            if len(removed_match_rows) == 0 and len(new_match_rows) == 0:
+                session_log.append('No changes since last time!')
+        
+        # If no previous data cache was found, create one and print all the fetched data
         else:
-            to_log.append('No previous data cache found. Creating a new cache for next run... All currently scheduled games are:')
-            all_game_rows = get_game_rows(df)
-            if len(all_game_rows) > 0:
-                to_log.extend(get_game_rows(df))
+            session_log.append('No previous data cache found. Creating a new cache for next run... All currently scheduled matches are:')
+            all_match_rows = generate_match_rows(df)
+            if len(all_match_rows) > 0:
+                session_log.extend(generate_match_rows(df))
 
         # Print and log results
-        print_rows(to_log)
-        write_rows_to_log(to_log)
+        print_rows(session_log)
+        write_rows_to_log(session_log)
 
         # Save latest data to file (Overwrite previous)
-        df.to_pickle(previous_data_file)
+        df.to_pickle(setup.previous_data_file)
     else:
-        print('Something went wrong. The games could not be fetched.')   
+        print('Something went wrong. The matches could not be fetched.')
+
+if __name__ == '__main__':
+    main()   
